aaa544ddd7ac842a50f3c0e1a437c10d0bcc5f51
Haohang Zhou <zhouhaohangau@gmail.com>
2021-05-18 19:17:10 +1000

    Added assignment file

---
+++ Assignment-3 Files-20210518/Assignment3_Template.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+  <SafeAreaView style={styles.container}>
+
+
+  </SafeAreaView>
+  );}
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffcc'
+    },
+  });
